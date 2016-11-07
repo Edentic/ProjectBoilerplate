78d46c25702336eade67700db444f265cc5e832d
Kasper Baagø Jensen <kasper@edentic.dk>
2016-11-07 09:11:40 +0100

    Renewing admin password on create, so it don´t expire

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -3,4 +3,5 @@
 DB=$1;
 
 #mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS \`$DB\`";
+mysqladmin -uhomestead -psecret password secret;
 mysql -uhomestead -psecret -e "CREATE DATABASE IF NOT EXISTS \`$DB\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci";
